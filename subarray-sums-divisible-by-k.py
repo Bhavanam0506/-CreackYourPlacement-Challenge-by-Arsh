2d1be722613e0942f3abf8ed583e595073d06621
Bhavanam0506 <92242002+Bhavanam0506@users.noreply.github.com>
2022-05-06 15:55:31 +0530

    Create subarray-sums-divisible-by-k.py

---
+++ subarray-sums-divisible-by-k.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        counts=[0]*k
+        sum=0
+        for x in nums:
+            sum+=x%k
+            counts[sum%k]+=1
+        r=counts[0]
+        for c in counts:
+            r+=(c*(c-1))//2
+        return(r)
+        
